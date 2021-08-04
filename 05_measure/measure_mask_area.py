e8641f9bcaac561ee2f7e273ed24b77b4536ba1a
James Hickson <jameshickson1497@gmail.com>
2021-08-04 16:14:18 +0100

    Got the vectorizing and area calculation sorted. Got necesary information extracted from the polygons, now need to write to csv

--- 05_measure/measure_mask_area.py
+++ 05_measure/measure_mask_area.py
@@ -5,6 +5,7 @@ import argcomplete, argparse
 from argcomplete.completers import ChoicesCompleter, FilesCompleter
 import csv
 from csv import DictWriter
+from datetime import datetime
 import difflib
 import gdal
 import geopandas as gpd
@@ -27,22 +28,19 @@ formatter_class=argparse.RawDescriptionHelpFormatter)
 # Functions:
 #----------------------------------------------------------------------------------------------------
 def vectorize(mask):
-    if os.path.exists(os.path.split(mask)[0] + "/06" + os.path.basename(os.path.splitext(mask)[0])[2:] + ".shp"):
-        os.remove(os.path.split(mask)[0] + "/06" + os.path.basename(os.path.splitext(mask)[0])[2:] + ".cpg")
-        os.remove(os.path.split(mask)[0] + "/06" + os.path.basename(os.path.splitext(mask)[0])[2:] + ".dbf")
-        os.remove(os.path.split(mask)[0] + "/06" + os.path.basename(os.path.splitext(mask)[0])[2:] + ".prj")
-        os.remove(os.path.split(mask)[0] + "/06" + os.path.basename(os.path.splitext(mask)[0])[2:] + ".shp")
-        os.remove(os.path.split(mask)[0] + "/06" + os.path.basename(os.path.splitext(mask)[0])[2:] + ".shx")
-        os.system("gdal_polygonize.py %s %s -b 1 %s"%(mask, os.path.split(mask)[0] + "/06" + os.path.basename(os.path.splitext(mask)[0])[2:] + ".shp", "/06" + os.path.basename(os.path.splitext(mask)[0])[2:]))
+    if os.path.exists(os.path.split(mask)[0] + "/06" + os.path.basename(os.path.splitext(mask)[0])[2:] + ".geojson"):
+        os.remove(os.path.split(mask)[0] + "/06" + os.path.basename(os.path.splitext(mask)[0])[2:] + ".geojson")
+        os.system("gdal_polygonize.py %s %s -b 1 %s"%(mask, os.path.split(mask)[0] + "/06" + os.path.basename(os.path.splitext(mask)[0])[2:] + ".geojson", "/06" + os.path.basename(os.path.splitext(mask)[0])[2:]))
     else:
-        os.system("gdal_polygonize.py %s %s -b 1 %s"%(mask, os.path.split(mask)[0] + "/06" + os.path.basename(os.path.splitext(mask)[0])[2:] + ".shp", "/06" + os.path.basename(os.path.splitext(mask)[0])[2:]))
-    return(os.path.split(mask)[0] + "/06" + os.path.basename(os.path.splitext(mask)[0])[2:] + ".shp")
+        os.system("gdal_polygonize.py %s %s -b 1 %s"%(mask, os.path.split(mask)[0] + "/06" + os.path.basename(os.path.splitext(mask)[0])[2:] + ".geojson", "/06" + os.path.basename(os.path.splitext(mask)[0])[2:]))
+
+    return(os.path.split(mask)[0] + "/06" + os.path.basename(os.path.splitext(mask)[0])[2:] + ".geojson")
 
 def area_calculator(shapefile):
     gdf = gpd.read_file(shapefile)
     cea = gdf["geometry"].to_crs({"proj":"cea"})
     gdf['Area'] = cea.area / 10 ** 6
-    gdf.to_file(shapefile)
+    gdf.to_file(shapefile, driver='GeoJSON')
 
 def selector(shapefile):
     # List of files
@@ -51,21 +49,43 @@ def selector(shapefile):
     product = ''.join(difflib.get_close_matches(os.path.split(shapefile)[1].rsplit('_', 4)[0].rsplit('.', 7)[2], files))
     # Filepath reconstructed.
     filepath = '/'.join(files[0:files.index(product)]) + "/" + product + "/"
-
-
-
+    # Read shapefile
     gdf = gpd.read_file(shapefile)
-    ds = gdf['Area'] > 200
-    if len(ds) >= 1:
-        filename = os.path.split(shapefile)[1].rsplit('_', 4)[0][3:]
-        doy = os.path.split(shapefile)[1].rsplit('_', 4)[0].rsplit('.', 7)[3][5:]
-        year = os.path.split(shapefile)[1].rsplit('_', 4)[0].rsplit('.', 7)[3][1:-3]
-        tile = os.path.split(shapefile)[1].rsplit('_', 4)[0].rsplit('.', 7)[4]
-        version = os.path.split(shapefile)[1].rsplit('_', 4)[0].rsplit('.', 7)[5]
-        minarea = min(gdf['Area'])
-        maxarea = max(gdf['Area'])
+    outputlist = []
+    # If area is more than X km2, collect file information.
+    if len(gdf['Area'] > 200) >= 1:
+        # This itterates through if there are > 1 polygon that meets the criteria.
+        for i in range(len(gdf.bounds)):
+            date = datetime.strptime(os.path.split(shapefile)[1].rsplit('_', 4)[0].rsplit('.', 7)[3][1:-3] + "-" + os.path.split(shapefile)[1].rsplit('_', 4)[0].rsplit('.', 7)[3][5:], "%Y-%j").strftime("%Y-%m-%d")
+            tile = os.path.split(shapefile)[1].rsplit('_', 4)[0].rsplit('.', 7)[4]
+            area = gdf['Area'][i]
+            filename = os.path.basename(shapefile).rsplit('_', 4)[0][3:]
+            minx = gdf.bounds.iloc[i][0]
+            miny = gdf.bounds.iloc[i][1]
+            maxx = gdf.bounds.iloc[i][2]
+            maxy = gdf.bounds.iloc[i][3]
+            outputlist.append([date, tile, area, filename, minx, miny, maxx, maxy])
 
-    return([filename, doy, year, tile, version, minarea, maxarea])
+    # Here I have written the following outputs to be inputted into a list. This essentially itterates through polygon and extract specific information and saves each one as a new list. (so list in a list).
+    # if you do a print(outputlist) it'll be very clear.
+    # Things to do:
+    # - Code so the csv is being saved in a suitable location - lines 47 to 51 basically do that, where it originally looks for the product name in the filename and looks for it in the path.
+    # - This may need to be done out of the function and done with the code below. (This should be done on the VM, so it works and is solid.)
+    # - Where there are lists in lists, each element should be written to a new column every time and when it is complete, it should then go to a new line.
+    # - Header for CSV are on line 148.
+
+
+
+    '''
+    with open('test_csv.csv', 'w') as f1:
+        writer=csv.writer(f1, delimiter='\t',lineterminator='\n',)
+        for elem in outputlist:
+            for i in elem:
+                writer.writerow(map(lambda x: [x], i))
+
+        sys.exit()
+    '''
+    #return([date, tile, area, filename, minx, miny, maxx, maxy])
 
 
 
@@ -105,16 +125,6 @@ def append_data(data, dataframe):
     row.append("filename", "DOY", "Year", "Tile", "Version", "Minimum Area (km2)", "Maximum Area (km2)")
     print(row)
     sys.exit()
-
-
-    print(filename)
-    print(doy)
-    print(year)
-    print(tile)
-    print(version)
-    print(minarea)
-    print(maxarea)
-    sys.exit()
     """
 #==========================================================
 # main:
@@ -135,14 +145,15 @@ if __name__ == "__main__":
         #----------------------------------------------------------------------------------------------------
         # Code:
         #----------------------------------------------------------------------------------------------------
-        df = pd.DataFrame(columns=("filename", "DOY", "Year", "Tile", "Version", "Minimum Area (km2)", "Maximum Area (km2)"))
+        df = pd.DataFrame(columns=("Date", "Tile", "Area", "Filename", "minx", "miny", "maxx", "maxy"))
+        df.to_csv('test_csv.csv')
         for img in args.input_img:
             vector = vectorize(img)
             area = area_calculator(vector)
-            #print(area)
             select = selector(vector)
-            save = append_data(select, df)
-        print(df)
+            print(select)
+            #save = append_data(select, df)
+        #print(df)
 
 
         #print(select)
