09ffe8e64d519c97229c1fd3ac68f4f55c126fde
James <jameshickson1497@gmail.com>
2021-08-16 15:20:20 +0000

    All appropriate information is being extracted from the geojson, just need to write to csv

--- 05_measure/measure_mask_area.py
+++ 05_measure/measure_mask_area.py
@@ -4,13 +4,14 @@
 import argcomplete, argparse
 from argcomplete.completers import ChoicesCompleter, FilesCompleter
 import csv
-from csv import DictWriter
+from csv import DictWriter 
 from datetime import datetime
 import difflib
 import gdal
 import geopandas as gpd
 import os, sys
 import pandas as pd
+from pathlib import Path
 #--------------------------------------------------------------------------------
 # Script description:
 #--------------------------------------------------------------------------------
@@ -46,7 +47,7 @@ def selector(shapefile):
     # List of files
     files = os.path.split(shapefile)[0].rsplit('/')
     # The product in the file path - where to save the csv to.
-    product = ''.join(difflib.get_close_matches(os.path.split(shapefile)[1].rsplit('_', 4)[0].rsplit('.', 7)[2], files))
+    product = ''.join(difflib.get_close_matches(os.path.split(shapefile)[1].rsplit('_', 4)[0].rsplit('.', 7)[1], files))
     # Filepath reconstructed.
     filepath = '/'.join(files[0:files.index(product)]) + "/" + product + "/"
     # Read shapefile
@@ -56,7 +57,9 @@ def selector(shapefile):
     if len(gdf['Area'] > 200) >= 1:
         # This itterates through if there are > 1 polygon that meets the criteria.
         for i in range(len(gdf.bounds)):
-            date = datetime.strptime(os.path.split(shapefile)[1].rsplit('_', 4)[0].rsplit('.', 7)[3][1:-3] + "-" + os.path.split(shapefile)[1].rsplit('_', 4)[0].rsplit('.', 7)[3][5:], "%Y-%j").strftime("%Y-%m-%d")
+            #date = datetime.datetime.strptime(os.path.split(shapefile)[1].rsplit('_', 4)[2].rsplit('.', 7)[3][1:-3] + "-" + os.path.split(shapefile)[1].rsplit('_', 4)[0].rsplit('.', 7)[3][5:], "%Y-%j").strftime("%Y-%m-%d")
+            date = datetime.strptime(os.path.split(shapefile)[1].rsplit('_', 4)[0].rsplit('.', 7)[2][1:], "%Y%j").date()
+            date.strftime("%Y-%m-%d")
             tile = os.path.split(shapefile)[1].rsplit('_', 4)[0].rsplit('.', 7)[4]
             area = gdf['Area'][i]
             filename = os.path.basename(shapefile).rsplit('_', 4)[0][3:]
@@ -65,8 +68,8 @@ def selector(shapefile):
             maxx = gdf.bounds.iloc[i][2]
             maxy = gdf.bounds.iloc[i][3]
             outputlist.append([date, tile, area, filename, minx, miny, maxx, maxy])
-    print(outputlist)
-    sys.exit()
+    return outputlist
+    
     # Here I have written the following outputs to be inputted into a list. This essentially itterates through polygon and extract specific information and saves each one as a new list. (so list in a list).
     # if you do a print(outputlist) it'll be very clear.
     # Things to do:
@@ -75,19 +78,7 @@ def selector(shapefile):
     # - Where there are lists in lists, each element should be written to a new column every time and when it is complete, it should then go to a new line.
     # - Header for CSV are on line 148.
 
-
-
-    '''
-    with open('test_csv.csv', 'w') as f1:
-        writer=csv.writer(f1, delimiter='\t',lineterminator='\n',)
-        for elem in outputlist:
-            for i in elem:
-                writer.writerow(map(lambda x: [x], i))
-
-        sys.exit()
-    '''
-    #return([date, tile, area, filename, minx, miny, maxx, maxy])
-
+   
 
 
 def append_data(data, dataframe):
@@ -99,34 +90,6 @@ def append_data(data, dataframe):
     # writing to csv could probably be easier as a column name is not necesarilly required
 
 
-
-    """
-    #with open(filepath + product[3:] + "_datadownload.csv", "w") as csv:
-    with open(filepath + product[3:] + "_datadownload.csv", 'a+', newline='') as write_obj:
-        field_names = ["filename", "DOY", "Year", "Tile", "Version", "Minimum Area (km2)", "Maximum Area (km2)"]
-        content_dict = {"filename":filename, "DOY":doy, "Year":year, "Tile":tile, "Version":version, "Minimum Area (km2)":minarea, "Maximum Area (km2)":maxarea}
-
-        # Create a writer object from csv module
-        dict_writer = DictWriter(write_obj, fieldnames=field_names)
-        dict_writer.writeheader()
-        # Add dictionary as wor in the csv
-        dict_writer.writerow(content_dict)
-        '''
-        # Create a writer object from csv module
-        csv_writer = writer(write_obj)
-        # Add contents of list as last row in the csv file
-        csv_writer.writerow(content)
-        '''
-
-    sys.exit()
-
-
-    r = csv.reader(csv)
-    #row = r.next()
-    row.append("filename", "DOY", "Year", "Tile", "Version", "Minimum Area (km2)", "Maximum Area (km2)")
-    print(row)
-    sys.exit()
-    """
 #==========================================================
 # main:
 #----------------------------------------------------------
@@ -135,7 +98,7 @@ if __name__ == "__main__":
         #----------------------------------------------------------------------------------------------------
         # Arguments:
         #----------------------------------------------------------------------------------------------------
-        parser.add_argument("-i", "--input-img", nargs="+", required=True, help="").completer = FilesCompleter(allowednames=(".tif"))
+        parser.add_argument("-i", "--input-img", nargs="+", required=True, help="Eroded and dilated heatmap image.").completer = FilesCompleter(allowednames=(".tif"))
         argcomplete.autocomplete(parser)
         args = parser.parse_args()
 
@@ -146,12 +109,64 @@ if __name__ == "__main__":
         #----------------------------------------------------------------------------------------------------
         # Code:
         #----------------------------------------------------------------------------------------------------
-        df = pd.DataFrame(columns=("Date", "Tile", "Area", "Filename", "minx", "miny", "maxx", "maxy"))
-        df.to_csv('test_csv.csv')
         for img in args.input_img:
+            # Split the file path into individual elements.
+            files = os.path.split(img)[0].rsplit('/')
+            # Direct filepath to the product and version.
+            product = ''.join(difflib.get_close_matches(os.path.split(img)[1].rsplit('_', 4)[0].rsplit('.', 7)[1], files))
+            # Reconstructed filepath.
+            filepath = '/'.join(files[0:files.index(product)]) + "/" + product + "/"
+            if not os.path.exists(filepath + "01_csv/"):
+                os.mkdir(filepath + "01_csv/")
+            if not os.path.exists(str(filepath + "01_csv/" + img.rsplit(".", 6)[1][1:-3]) + "_imgs_in_criteria.csv"):
+                Path(str(filepath + "01_csv/" + img.rsplit(".", 6)[1][1:-3]) + "_imgs_in_criteria.csv").touch()
+                
+                headers = ["Date", "Tile", "Area", "Filename", "minx", "miny", "maxx", "maxy"]
+
+                with open(str(filepath + "01_csv/" + img.rsplit(".", 6)[1][1:-3]) + "_imgs_in_criteria.csv", "w+") as f:
+                    writer = csv.DictWriter(f, fieldnames=headers)
+                    writer.writeheader()
+                
+
+                # write to csv is working, but for some reason the column headers don't write.......
+
+
+
+
+
+
+
+
+
+
+                sys.exit()
+                csvfile = open(str(filepath + "01_csv/" + img.rsplit(".", 6)[1][1:-3]) + "_imgs_in_criteria.csv", "w+")
+                #with open(str(filepath + "01_csv/" + img.rsplit(".", 6)[1][1:-3]) + "_imgs_in_criteria.csv") 
+                with open(csvfile) as f:
+                    writer = csv.DictWriter(f, fieldnames=headers)
+                    writer.writeheader()
+
+
+
+
+
+            # Works to create csv file        
+                #df = pd.DataFrame(columns=("Date", "Tile", "Area", "Filename", "minx", "miny", "maxx", "maxy"))
+                #df.to_csv(str(filepath + "01_csv/" + img.rsplit(".", 6)[1][1:-3]) + "_imgs_in_criteria.csv")
+            
+
+        for img in args.input_img:
+            # Vectorize image.
             vector = vectorize(img)
+            # Calculate area of polygons in shapefile.
             area = area_calculator(vector)
+            # Selects those which fit in the criteria (i.e. area <= 200km2).
             select = selector(vector)
+            csvfile = open(str(filepath + "01_csv/" + img.rsplit(".", 6)[1][1:-3]) + "_imgs_in_criteria.csv", "w+", newline="")
+            with csvfile:
+                csv.writer(csvfile).writerows(select)
+                
+            sys.exit()
             print(select)
             #save = append_data(select, df)
         #print(df)
