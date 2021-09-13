fdf9819a985c1a78986b7bb9970a6f61ee236a5c
James <jameshickson1497@gmail.com>
2021-09-13 15:14:40 +0000

    Fixed having to run the script twice. Imported the array into the erode and dilation function, so rather than importing it again it reads it direct

--- 04_identify/polynya_identification_heatmap.py
+++ 04_identify/polynya_identification_heatmap.py
@@ -31,27 +31,27 @@ def cumval(x):
     # Adds up all values.
     return x.sum()
 
-def erosion_dialation(heatmap, eroded_output):
-    # Read image.
-    array = rasterio.open(heatmap).read(1)
+def erosion_dialation(array, eroded_output):
     # Create an array of zeroes and another of ones.
     arrZero, arrOne = np.zeros(array.shape), np.ones(array.shape)
     # Where the array has a value more than 5 make the arrOne = True, and everywhere else (False) = arrZero.
     array_mod = np.where(array>5, arrOne, arrZero)
-    profile = rasterio.open(heatmap).profile
-    profile.update({'dtype':'uint8'})
+    #profile.update({'dtype':'uint8'})
     profile.update({'nodata':'0'})
     # Set structure - on how the array is being checked.
     structure = ndimage.generate_binary_structure(2, 2)
     # Erode, dialate and erode again.
-    print("Eroding & dilating...")
+    print(f"Eroding & dilating {os.path.split(input)[1]}...")
     array_mod = morpho.binary_erosion(array_mod, structure=structure, iterations=1, border_value=0)
-    array_mod = morpho.binary_dilation(array_mod, structure=structure, iterations=2,border_value=0)
-    array_mod = morpho.binary_erosion(array_mod, structure=structure,iterations=1, border_value=0)
+    array_mod = morpho.binary_dilation(array_mod, structure=structure, iterations=2, border_value=0)
+    array_mod = morpho.binary_erosion(array_mod, structure=structure, iterations=1, border_value=0)
+
     # Select the appropriate data.
     array_mod = np.where(array_mod,1,0)
+    array_mod = np.array(array_mod, dtype=np.uint8)
     with rasterio.open(eroded_output, 'w', **profile) as imgout:
-        imgout.write(array_mod.astype('uint8'), 1)
+        imgout.write(array_mod, 1)
+        
 
 #==========================================================
 # main:
@@ -74,20 +74,22 @@ if __name__ == "__main__":
         # Code:
         #----------------------------------------------------------------------------------------------------
         for input in args.input_img:
+            # Read image
+            with rasterio.open(input, 'r') as threshold_class:
+                threshold_array = threshold_class.read(1)
+                profile = threshold_class.profile
             # Itterate through the image and calculate the sum of all pixels in the 3x3 box.
             # The 'constant' gives the values which lie on the size of the image a value of 0.
-            window_filter = ndimage.generic_filter(gdal.Open(input).ReadAsArray(), cumval, size=(3,3), mode='constant')
-            #Set output information (stored from input image)
-            driver = gdal.GetDriverByName("GTiff")
-            outImg = driver.Create(os.path.split(input)[0] + "/04" + os.path.basename(os.path.splitext(input)[0])[2:] + "_heatmap.tif", gdal.Open(input).RasterYSize, gdal.Open(input).RasterXSize, 1, gdal.GDT_Byte)
-            outBand = outImg.GetRasterBand(1)
-            outBand.WriteArray(window_filter)
-            outImg.SetGeoTransform(gdal.Open(input).GetGeoTransform())
-            outImg.SetProjection(gdal.Open(input).GetProjection())
-            del window_filter, driver, outImg, outBand
-
+            window_filter = ndimage.generic_filter(threshold_array, cumval, size=(3,3), mode='constant')
+            
+            # Heatmap saved for future purposes. 
+            heatmap_output = os.path.join(os.path.split(input)[0], "04" + os.path.basename(os.path.splitext(input)[0])[2:] + "_heatmap.tif")
+            with rasterio.open(heatmap_output, 'w', **profile) as h_output:
+                h_output.write(window_filter, 1)
+            
             # Erode and dialate the heatmap.
-            erosion_dialation(os.path.split(input)[0] + "/04" + os.path.basename(os.path.splitext(input)[0])[2:] + "_heatmap.tif", os.path.split(input)[0] + "/05" + os.path.basename(os.path.splitext(input)[0])[2:] + "_heatmap_eroded.tif")
+            erosion_dialation(window_filter, os.path.split(input)[0] + "/05" + os.path.basename(os.path.splitext(input)[0])[2:] + "_heatmap_eroded.tif")
+            
         #----------------------------------------------------------------------------------------------------
         # Run and errors:
         #----------------------------------------------------------------------------------------------------
