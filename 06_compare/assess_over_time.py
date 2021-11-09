c2e7c26220535bbd59a2a4d3c9250c444b7ae790
James <jameshickson1497@gmail.com>
2021-11-09 16:05:57 +0000

    implement the use of start and end dates, and time. This will be used to select appropriate imagery

--- 06_compare/assess_over_time.py
+++ 06_compare/assess_over_time.py
@@ -6,6 +6,7 @@ from argcomplete.completers import ChoicesCompleter, FilesCompleter
 from datetime import datetime, timedelta
 import gdal
 import numpy as np
+from PIL import Image
 import sys, os, glob
 
 #--------------------------------------------------------------------------------
@@ -36,18 +37,55 @@ if __name__ == "__main__":
         #----------------------------------------------------------------------------------------------------
         # Arguments:
         #----------------------------------------------------------------------------------------------------
-        parser.add_argument("-i", "--input-img", nargs="+", required=True, help="").completer = FilesCompleter(allowednames=(".tif"))
-        parser.add_argument("-d", "--days", required=True, type=int, help="Number of days to look at from input image.")
+        parser.add_argument("-i", "--input-img", nargs="+", help="").completer = FilesCompleter(allowednames=(".tif"))
+        parser.add_argument("-d", "--days", type=int, help="Number of days to look at from input image.")
+        parser.add_argument("-t", "--tile", help="Filepath to MODIS tile of interest.")
+        parser.add_argument("-s", "--time-start", help="Time lower bound (YYYY/MM/DD)")
+        parser.add_argument("-e", "--time-end", help="Time upper bound (YYYY/MM/DD)")
         argcomplete.autocomplete(parser)
         args = parser.parse_args()
 
         #----------------------------------------------------------------------------------------------------
         # Check for Errors:
         #----------------------------------------------------------------------------------------------------
-
+        #if args.tile ends with '/' remove it!
+        # time-end or days must be specified!
+        #make sure date is split with "/" or if it is as '-' then change it?
         #----------------------------------------------------------------------------------------------------
         # Code:
         #----------------------------------------------------------------------------------------------------
+        tile = args.tile
+        sdate = datetime.strptime(os.path.join(args.time_start), "%Y/%m/%d").date()
+        # End date based on specified or calculated from the start date from the number of days. 
+        if args.time_end:
+            edate = datetime.strptime(os.path.join(args.time_end), "%Y/%m/%d").date()
+        elif args.days:
+            edate = sdate + timedelta(days=args.days)
+        #else:
+        #    raise RuntimeError("Please specify either an end-date using '-e' or a number of days from the start date using '-d'.")
+
+        # Acquire list of all the dates.
+        date_list = [sdate + timedelta(days=x) for x in range((edate-sdate).days + 1)]
+        # Full directory (including tile & date).
+        directory_dates = ["/".join((tile, str(d.year), str('%02d' %d.month), str('%02d' %d.day))) for d in date_list if os.path.exists("/".join((tile, str(d.year), str('%02d' %d.month), str('%02d' %d.day))))]
+        # List of directory and filenames.
+        full_filepath = [files for dir in directory_dates for files in glob.glob(os.path.join(dir, "08*.tif"))]
+        # Extract array of images within the date range that are available.
+        array = np.array([np.array(Image.open(fimg)) for fimg in full_filepath])
+        cumulative_array = np.sum(np.stack(array), axis=0)
+        
+        print(array.shape)
+        sys.exit()
+
+        
+        
+        
+        print(tile)
+        print(time_start)
+        print(time_end)
+        print("".join((tile, time_start)))
+        sys.exit()
+        
         for img in args.input_img:
             # Split path to get date.
             path_split = os.path.split(img)[0].rsplit('/')
@@ -63,23 +101,32 @@ if __name__ == "__main__":
             date_list = [start_date + timedelta(days=x) for x in range((end_date-start_date).days + 1)]
             # Generate filepaths for all the dates.
             dir_date_list = ["/".join((tile_filepath, str(d.year), str('%02d' %d.month), str('%02d' %d.day))) for d in date_list if os.path.exists("/".join((tile_filepath, str(d.year), str('%02d' %d.month), str('%02d' %d.day))))]
-            # Pull files for all dates.
-            files_date_list = ["/".join((fp, i)) for fp in dir_date_list for i in glob.glob(os.path.join(fp, "08*.tif"))]
+            # Pull files for all dates.     
+            files_date_list = [files for dir in dir_date_list for files in glob.glob(os.path.join(dir, "08*.tif"))]
             
-            # PRINT ALL VALUES FOR THE DATE RANGE
-            # CHECK 05 FILTER script with "writing to an empty dataframe"
-            print(files_date_list)
+            array = np.array([np.array(Image.open(fimg)) for fimg in files_date_list])
+            cumulative_array = np.sum(np.stack(array), axis=0)
+            print(array.shape)
             sys.exit()
             
-            for fp in file_date_list:
-                for i in glob.glob(os.path.join(fp, "08*")):
-                    print(i)
-                    sys.exit()
-                   
-                
+
+
+
+            # write to image for viewing purposes
+            cols = gdal.Open(img).RasterXSize
+            rows = gdal.Open(img).RasterYSize
+            proj = gdal.Open(img).GetProjection()
+            geom = gdal.Open(img).GetGeoTransform()
+
+            outDataset = gdal.GetDriverByName("GTiff").Create("test.tif", cols, rows, 1, gdal.GDT_Float32)
+            outDataset.SetProjection(proj)
+            outDataset.SetGeoTransform(geom)
+            outBand = outDataset.GetRasterBand(1)
+            outBand.WriteArray(cumulative_array) 
             
+            sys.exit()
             
-            
+                    
             
             sys.exit()
             #array = img_to_array(img)
