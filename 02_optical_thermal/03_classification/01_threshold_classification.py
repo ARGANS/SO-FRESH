b524d4af39d4cd851adeab5d4157002609986309
James <jameshickson1497@gmail.com>
2022-02-24 12:00:54 +0000

    Implement the threshold classification for the optical side and hard set for the thermal

--- 02_optical_thermal/03_classification/01_threshold_classification.py
+++ 02_optical_thermal/03_classification/01_threshold_classification.py
@@ -18,7 +18,7 @@ from sklearn.neighbors import KernelDensity
 # Script description:
 #--------------------------------------------------------------------------------
 parser = argparse.ArgumentParser(description="""
-# Undertake a basic threshold classification.
+# Undertake a classification of fused data. 
 **************************************************************************
 ##Tasks:
 -  Threshold classification where the upper limit value is set.
@@ -29,17 +29,13 @@ formatter_class=argparse.RawDescriptionHelpFormatter)
 # Functions:
 #----------------------------------------------------------------------------------------------------
 class thresholding:
-    def __init__(self, data, default_threshold=180, max_thresh=200, min_thresh=160):
-        self.data = gdal.Open(data).ReadAsArray()
+    def __init__(self, data_array, default_threshold=180, max_thresh=185, min_thresh=170):
+        self.data_array = data_array
         self.default_threshold = default_threshold
         self.min_thresh = min_thresh
         self.max_thresh = max_thresh
         self.x = None
         self.y = None
-    
-    def greyscale(self):
-        # Returns greyscale of input image (all array bands are summed and divided by total number)
-        return np.sum(self.data, axis=0)/(self.data.shape[0]) 
 
     def otsu_threshold(self, bins):
         # Thresholding using Otsu's method
@@ -51,7 +47,6 @@ class thresholding:
                 return self.default_threshold
         except:
             return self.default_threshold
-
     def min_threshold(self, bins=256):
         try:
             t = threshold_minimum(image=array, nbins=bins)
@@ -62,10 +57,30 @@ class thresholding:
         except:
             return self.default_thresh
 
-def threshold_classify(input, output, threshold):
-    tmpout = output[:-4] + "thresholdtmp.tif"
-    os.system("gdal_calc.py --quiet -A %s --allBands=A --outfile=%s --calc='A<=%s' --overwrite"%(input, tmpout, threshold))
-    os.system("gdal_translate -q -b 1 %s %s"%(tmpout, output))
+def greyscale(array):
+    # Returns greyscale of input image (all array bands are summed and divided by total number)
+    return np.sum(array, axis=0)/(array.shape[0]) 
+
+def threshold_classify(input, opt_thresh=None, opt_bands=None, therm_thresh=None, therm_bands=None, n_bands=None):
+    out_img = os.path.split(input)[0]+"/03a"+os.path.basename(os.path.splitext(input)[0])[2:]+"_classification_o"+str(opt_thresh)+"_t"+str(therm_thresh)+".tif"
+    tmpout = out_img[:-4] + "_tmpfile.tif"
+    # Assign letters based on band numbers.
+    if opt_bands or therm_bands != None:
+        opt_bands_let = [chr(ord("@")+x) for x in opt_bands]
+        therm_bands_let = [chr(ord("@")+x) for x in therm_bands]
+    # All band numbers and corresponding letters.
+    band_numbs = sorted(list(opt_bands+therm_bands))
+    band_lets = sorted(list(opt_bands_let+therm_bands_let))
+    # Command inputs.
+    band_expression = ' '.join([f"-{l} {input} --{l}_band={n}" for n, l in zip(band_numbs, band_lets)]) 
+
+    opt_calc = "*".join([f"({b}>1)*({b}<={opt_thresh})" for b in opt_bands_let])
+    therm_calc = "*".join([f"({b}>={therm_thresh})" for b in therm_bands_let])
+    # Calculation expression.
+    calc_expression = opt_calc+" + "+therm_calc
+
+    os.system("gdal_calc.py --quiet %s --outfile=%s --calc='%s' --overwrite"%(band_expression, tmpout, calc_expression))
+    os.system("gdal_translate -q -b 1 %s %s"%(tmpout, out_img))
     os.remove(tmpout)
 
 def img_viewer(input):
@@ -81,7 +96,7 @@ if __name__ == "__main__":
         # Arguments:
         #----------------------------------------------------------------------------------------------------
         parser.add_argument("-i", "--input-img", nargs="+", required=True, help="Input image - image to be classified. (Will begin with '02'.)").completer = FilesCompleter(allowednames=(".tif"))
-        #parser.add_argument("-t", "--threshold", required=True, type=int, help="The upper threshold value as an interger.")
+        #parser.add_argument("-t", "--threshold", required=False, type=int, help="The upper threshold value as an interger.")
         argcomplete.autocomplete(parser)
         args = parser.parse_args()
 
@@ -92,35 +107,35 @@ if __name__ == "__main__":
         #----------------------------------------------------------------------------------------------------
         # Code:
         #----------------------------------------------------------------------------------------------------
-        # tasks to do:
-        # read image, check products in path name.
-        # maybe hard code some functions for the optical and thermal products
-        # check bands variation in values? 
-        # run gdal_calc on it for classification, change from all bands to 123 optical and 4 thermal.
-        # boom classification
-        for img in args.input_img:
-            
-            init = thresholding(img)
-            #arr = init.greyscale()
-            print(init)
-            print(init.otsu_threshold(bins=128))
-
-            #print(array)
-            #print(otsu)
-            #mini = init.min_threshold()
-            sys.exit()
-            #'''
-
-            plt.hist(array)
-            plt.axvline(x=(otsu))
-            plt.show()
-            sys.exit()
-            #'''
-            print(test.otsu_threshold(bins=256))
-        sys.exit()
         print(f"Undertaking threshold classification for {len(args.input_img)} images")
         for img in tqdm(args.input_img):
-            threshold_classify(img, os.path.split(img)[0] + "/03" + os.path.basename(os.path.splitext(img)[0])[2:] + "_threshold_" + str(args.threshold) + ".tif", args.threshold)
+            products = [string for string in (os.path.basename(img).rsplit("_")) if "MYD" in string]
+            if len(products) == 2:
+                if gdal.Open(img).ReadAsArray().ndim == 3:
+                    img_array = gdal.Open(img).ReadAsArray()
+                    # Number of bands.
+                    bands_n = img_array.shape[0]
+                for p in enumerate(products):
+                    if p[1] == "MYD09GA":
+                        opt_arr = (img_array)[p[0]:int(p[0]+3)]
+                        #opt_arr_slice = (int(p[0]), int(p[0]+3))
+                        opt_arr_slice = [x for x in range(int(p[0]+1), int(p[0]+4))]
+                        opt_greyscale = greyscale(opt_arr)
+                    elif p[1] == "MYDTBGA":
+                        if opt_arr.any():
+                            therm_arr = (img_array)[int(p[0]+2):int(p[0]+3)]
+                            #therm_arr_slice = (int(p[0]+2), int(p[0]+3))
+                            therm_arr_slice = [x for x in range(int(p[0]+3), int(p[0]+4))]
+                        else:
+                            therm_arr = (img_array)[p[0]:int(p[0]+1)]
+                            #therm_arr_slice = (int(p[0]), int(p[0]+1))
+                            therm_arr_slice = [x for x in range(int(p[0]+1), int(p[0]+2))]
+
+
+            init = thresholding(opt_greyscale)
+            otsu_val = init.otsu_threshold(bins=128)
+
+            threshold_classify(img, opt_thresh=otsu_val, opt_bands=opt_arr_slice, therm_thresh=265, therm_bands=therm_arr_slice, n_bands=bands_n)
         print("Process complete - threshold classifications produced")
         #----------------------------------------------------------------------------------------------------
         # Run and errors:
