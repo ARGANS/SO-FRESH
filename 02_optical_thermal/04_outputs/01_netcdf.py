824bef58fb92ef31444be8f3f747b84aed573d56
James <jameshickson1497@gmail.com>
2022-03-09 12:12:27 +0000

    Added functionailty to clip the data to specific x & y extent as well as being able to set specific values to NaN

--- 02_optical_thermal/04_outputs/01_netcdf.py
+++ 02_optical_thermal/04_outputs/01_netcdf.py
@@ -5,11 +5,10 @@ import argcomplete, argparse
 from argcomplete.completers import ChoicesCompleter, FilesCompleter
 from datetime import datetime, timedelta
 import numpy as np
-import numpy.ma as ma
 import itertools
 from PIL import Image
 import rasterio
-import pprint, gdal
+import gdal
 import glob, sys, os
 import netCDF4 as nc
 
@@ -36,7 +35,6 @@ def array_to_img(array, coords):
     outDataset.SetProjection(proj)
     outDataset.SetGeoTransform(geom)
     outBand = outDataset.GetRasterBand(1)
-    #outBand.SetDescription("Frequency_count")
     outBand.WriteArray(array)
 
 #==========================================================
@@ -51,15 +49,25 @@ if __name__ == "__main__":
         parser.add_argument("-t", "--tile-folder", nargs="+", help="Opportunity to specify tiles of interest.")
         parser.add_argument("-s", "--time-start", required=True, help="Time lower bound (YYYY/MM/DD)")
         parser.add_argument("-e", "--time-end", required=True, help="Time upper bound (YYYY/MM/DD)")
-        parser.add_argument("-r", "--remove", required=False, action="store_true", help="Include to remove created 'tif' files")
-        #parser.add_argument("-d", "--days", type=int, help="Time upper bound (YYYY/MM/DD)")
         parser.add_argument("-o", "--output-dir", required=True, help="Output directory.")
+        parser.add_argument("-r", "--remove", required=False, action="store_true", help="Include to remove created 'tif' files")
+        parser.add_argument("-c", "--clip", required=False, action="store_true", help="Time upper bound (YYYY/MM/DD)")
+        parser.add_argument("-ymin", "--y-min", required=False, type=int, help="Minimum latitude to clip the data, if left empty, this will take the data extent.")
+        parser.add_argument("-ymax", "--y-max", required=False, type=int, help="Maximum latitude to clip the data, if left empty, this will take the data extent.")
+        parser.add_argument("-xmin", "--x-min", required=False, type=int, help="Minimum longitude to clip the data, if left empty, this will take the data extent.")
+        parser.add_argument("-xmax", "--x-max", required=False, type=int, help="Maximum longitude to clip the data, if left empty, this will take the data extent.")
+        parser.add_argument("-z", "--zero-nan", required=False, action="store_true", help="Set all values equal to 0 to NaN.")
+        parser.add_argument("-n", "--nan", required=False, help="Set values equal to or more than this value to zero")
+
         argcomplete.autocomplete(parser)
         args = parser.parse_args()
 
         #----------------------------------------------------------------------------------------------------
         # Check for Errors:
         #----------------------------------------------------------------------------------------------------
+        if args.clip == True:
+            if args.y_min == None and args.y_max == None and args.x_min == None and args.x_max == None:
+                raise RuntimeError("If you wish to clip the data to a specific extent, please specify at least one extent you would like to clip to.")
 
         #----------------------------------------------------------------------------------------------------
         # Code:
@@ -89,7 +97,7 @@ if __name__ == "__main__":
                     pass
             if ffp_by_tile:
                 ffp_split.append(ffp_by_tile)
-
+        # Add image to the filepath in a dictionary where each key is a tile.
         img_dict = {}
         for fp in ffp_split:
             img_by_tile = []
@@ -103,7 +111,6 @@ if __name__ == "__main__":
                         img_dict[tile].append(i)
         # Remove key which do not any associated values.
         img_dict = {k : v for k, v in img_dict.items() if v}
-
         img_array = {}
         img_coords = {}
         for key in img_dict.keys():
@@ -131,17 +138,16 @@ if __name__ == "__main__":
             sum_arr = np.sum(np.stack(img_array[key]), axis=0)
             img_array[key] = sum_arr
 
+        # Pull product name for file names. If statement incase the filepath ends with a '/'.
+        if bool(args.filepath_tile.rsplit("/",2)[-1]) == True:product = args.filepath_tile.rsplit("/",2)[-1]
+        else:product = args.filepath_tile.rsplit("/", 2)[-2]
+
         # Export stacked tile arrays to tifs files.
         print("Exporting images...")
         imgs_4_netcdf = []
         for key in img_array.keys():
-            #output_fp = "/".join((args.filepath_tile + "99_outputs", (args.time_start.replace("/", "") + "_" + args.time_end.replace("/", "")), ""))
-            #print(os.path.split(os.path.split(args.filepath_tile)[0])[1])
-            ### SORT OUT THIS PART with regards to the output!! ###
-
             output_fp = args.output_dir + (args.time_start.replace("/", "") + "_" + args.time_end.replace("/", "")+"/")
-            
-            out_file = os.path.join(output_fp + "tmp/" + "SOFRESH_MODIS_POLYNYA_" + args.time_start.replace("/", "") + "_" + args.time_end.replace("/", "") + "_" + key + ".tif")
+            out_file = os.path.join(output_fp + "tmp/" + "SOFRESH_MODIS_POLYNYA_" + product + "_" + args.time_start.replace("/", "") + "_" + args.time_end.replace("/", "") + "_" + key + ".tif")
             if not os.path.exists(output_fp):
                 os.mkdir(output_fp)
             if not os.path.exists(os.path.join(output_fp + "tmp/")):
@@ -151,20 +157,50 @@ if __name__ == "__main__":
 
         if not os.path.exists(os.path.join(output_fp + "00_netcdf/")):
             os.mkdir(os.path.join(output_fp + "00_netcdf/"))
-        #cmd = "gdal_merge.py -of GTIFF -o %s %s"%(os.path.join(output_fp + "tmp/" + "SOFRESH_MODIS_POLYNYA_" + args.time_start.replace("/", "") + "_" + args.time_end.replace("/", "") + ".tif"), ' '.join(imgs_4_netcdf))
 
         # Mosaic list of tif tiles to one netCDF file.
-        out_tif= os.path.join(output_fp + "tmp/" + "SOFRESH_MODIS_POLYNYA_" + args.time_start.replace("/", "") + "_" + args.time_end.replace("/", "") + ".tif")
-        out_nc = os.path.join(output_fp + "00_netcdf/" + "SOFRESH_MODIS_POLYNYA_" + args.time_start.replace("/", "") + "_" + args.time_end.replace("/", "") + ".nc")
+        out_tif= os.path.join(output_fp + "tmp/" + "SOFRESH_MODIS_POLYNYA_" + product + "_" + args.time_start.replace("/", "") + "_" + args.time_end.replace("/", "") + ".tif")
+        if args.clip == True:clip_tif = os.path.join(output_fp + "tmp/" + "SOFRESH_MODIS_POLYNYA_" + product + "_" + args.time_start.replace("/", "") + "_" + args.time_end.replace("/", "") + "_clipped.tif")
+        out_nc = os.path.join(output_fp + "00_netcdf/" + "SOFRESH_MODIS_POLYNYA_" + product + "_" + args.time_start.replace("/", "") + "_" + args.time_end.replace("/", "") + ".nc")
         # Merge all tif files in to one.
-        os.system("gdal_merge.py -q -of GTIFF -o %s %s"%(out_tif, ' '.join(imgs_4_netcdf)))
         print("Merging images...")
+        os.system("gdal_merge.py -q -of GTIFF -o %s %s"%(out_tif, ' '.join(imgs_4_netcdf)))
+        # If clip is specified, clip the tif before converting to netCDF file.
+        if args.clip == True:
+            img = gdal.Open(out_tif)
+            array = img.ReadAsArray()
+            ypixels, xpixels = array.shape[0], array.shape[1]
+            # Extract image geometry
+            gt = img.GetGeoTransform()
+            xmin, xres = gt[0], gt[1]
+            ymax, yres = gt[3], gt[5]
+            xmax = (xres*xpixels)+xmin
+            ymin = (yres*ypixels)+ymax
+            # Check if x or y limits have been set.
+            if bool(args.y_min) == True:ymin=args.y_min
+            if bool(args.y_max) == True:ymax=args.y_max
+            if bool(args.x_min) == True:xmin=args.x_min
+            if bool(args.x_max) == True:xmax=args.x_max
+            latbounds = [ymin, ymax]
+            lonbounds = [xmin, xmax]
+            ulx, uly, lrx, lry = lonbounds[0], latbounds[1], lonbounds[1], latbounds[0]
+            os.system("gdal_translate -q -of GTiff -projwin %s %s %s %s %s %s"%(ulx, uly, lrx, lry, out_tif, clip_tif))
+            # If required, set values to NaN.
+            if args.zero_nan == True or args.nan == True:
+                in_tif = clip_tif
+                with rasterio.open(in_tif) as src:
+                    array=src.read(1)
+                    profile = src.profile
+                if args.zero_nan == True:array[array==0] = np.nan
+                if args.nan == True:array[array>=args.nan] = np.nan
+                with rasterio.open(clip_tif, "w", **profile) as output:
+                    output.write(array, 1)
+            out_tif = clip_tif
         # Convert tif to netCDF.
         os.system("gdal_translate -q -of netCDF %s %s"%(out_tif, out_nc))
         print("NetCDF file successfully created.")
         # Change variable long name in netCDF file to "Count Frequency".
         os.system("ncatted -O -a long_name,Band1,o,c,Count_Frequency %s"%(out_nc))
-
 
         # Remove all '.tif' files created if specified.
         [os.remove(i) for i in imgs_4_netcdf if args.remove == True]
