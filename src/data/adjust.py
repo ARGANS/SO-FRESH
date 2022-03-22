f2dcd10f4574b1b753978af7993e569f15bc4fb6
James <jameshickson1497@gmail.com>
2022-03-22 12:30:37 +0000

    Implement mosaic method and resampling

--- src/data/adjust.py
+++ src/data/adjust.py
@@ -9,38 +9,49 @@ import os, sys
 
 parser = argparse.ArgumentParser(description="""
 ===================================================
-# Imagery pre-processing and adjustment.
+### Imagery pre-processing and adjustment. ###
 Pre-processing and adjustment options:
-- File renaming
-    - input: Add 01 at the begining of the raw downloaded files.
+- File renaming (add 01 at the begining of the file)
+    - input: raw downloaded file.
 - Project MYD09GA (optical).
     - input: 01_MYD09GA.jpg file.
 - Extract and normalise MYDTBGA (thermal).
     - input: 01_MYDTBGA.hdf file.
 - Extract and project AMSR2 SIC (Sea Ice Concentration).
-    - input: 01_
+    - input: 01_ESACCI_AMSR2_SIC.nc file.
+    - include '-r' for data to be resampled*
+===================================================
+### Imagery mosaic and resampling. ###
+For AMSR2 Ice Concentration - check step above.
+For MYD09GA & MYDTBGA:
+    - data_folder: filepath to folder containing all data in.
+    - start/end_date: lower and upper time threshold (each date will be an individual image).
+    - product: MYD09GA & MYDTBGA
+    - include '-r' for the data to be resampled.
 ===================================================""",
 formatter_class=argparse.RawDescriptionHelpFormatter)
 
 if __name__ == "__main__":
     try:
         parser.add_argument("-i", "--input", required=False, nargs="+", help="Provide filepath to the source of imagery you would like to process i.e. -i file/path/*/*/*.jpg")
-        parser.add_argument("-s", "--start-date", required=False, help"")
-        parser.add_argument("-e", "--end-date", required=False, help"")
-        parser.add_argument("-p", "--product", required=False, help"")
-        #parser.add_argument()
+        parser.add_argument("-d", "--data-folder", required=False, help="Filepath to the folder containing all data.")
+        parser.add_argument("-s", "--start-date", required=False, help="Time lower bound (YYYY/MM/DD).")
+        parser.add_argument("-e", "--end-date", required=False, help="Time upper bound (YYYY/MM/DD).")
+        parser.add_argument("-p", "--product", required=False, help="Product to merge [MYD09GA, MYDTBGA].")
+        parser.add_argument("-r", "--resample", required=False, action="store_true", help="For data to be resampled too, include '-r' in the command. This will currently be done to 0.1 (x & y) degrees resolution.")
+        #parser.add_argument("-t", "--tiles", required=False, nargs"+", help="")
         #parser.add_argument()
         argcomplete.autocomplete(parser)
         args = parser.parse_args()
 
         #==================================================================
         # Check for errors:
-
-
-
+        if not all([args.data_folder, args.start_date, args.end_date, args.product]):
+            raise RuntimeError("For mosaic creationg required inputs are missing.")
+        sys.exit()
         #==================================================================
         # Execute adjustments:
-        if args.input == True:
+        if bool(args.input) == True:
             for img in args.input:
                 filename = os.path.basename(img)
                 # Check if file requires to be renamed.
@@ -58,12 +69,16 @@ if __name__ == "__main__":
                 # Check if SIC requires to be extracted from netCDF.
                 elif filename.startswith("01_") and filename.endswith(".nc"):
                     sic_img = tk.amsr2_preprocess(img).extract_from_netcdf()
-                    tk.amsr2_preprocess(sic_img).reproject()
-        elif args.start_date==True and args.end_date==True and args.product==True:
-            # set up mosaic building
-            #   -dates & products
-            # resample
-            print('here')
+                    rpjct_sic_img = tk.amsr2_preprocess(sic_img).reproject(args.resample)
+                    if args.resample == True:
+                        tk.amsr2_preprocess(rpjct_sic_img).resample()
+        elif bool(args.data_folder)==True and bool(args.start_date)==True and bool(args.end_date)==True and bool(args.product)==True:
+            data_folder, sdate, edate, product=args.data_folder, args.start_date, args.end_date, args.product
+            images, dates= tk.modis_mosaic(data_folder, product).build_filepath(sdate, edate)
+            for i, d in zip(images, dates):
+                mosaic_img = tk.modis_mosaic(data_folder, product).build_mosaic(i, d, args.resample)
+                if args.resample == True:
+                    tk.modis_mosaic(data_folder, product).resample(mosaic_img)
         #==================================================================
         # Run and errors:
     except RuntimeError as msg:
