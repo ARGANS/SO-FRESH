e5be83148dd6ba27639acf10fb68c2e21b1ba35a
James <jameshickson1497@gmail.com>
2022-04-12 11:34:41 +0000

    add user interaction to specify optical version

--- src/data/fusion/toolkit.py
+++ src/data/fusion/toolkit.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # @James Hickson | Argans UK | jhickson@argans.co.uk
 """
-Toolkit for data fusion.
+Toolkit for data fusion application for the Automated Polynya Identification Tool
 """
 # packages
 import os, sys
@@ -21,10 +21,14 @@ def fusion(data_folder, sdate, edate, products):
     edate=datetime.strptime(os.path.join(edate), "%Y/%m/%d").date()
     date_dt=[sdate+timedelta(days=x) for x in range((edate-sdate).days+1)]
     dates = ["/".join((str(d.year), str("%02d" %d.month), str("%02d" %d.day))) for d in date_dt]
+    if any("MYD09GA" in p for p in products):
+        version = input("What MYD09GA version would you like, 006 or 061?:\n")
     for d in dates:
         imgs_4_fusion=[]
         for p in products:
-            if p == "MYD09GA" or p == "MYDTBGA":fp=glob.glob((data_folder+"MODIS/"+p+"_006"+"/02_mosaic/"+d+"/02_*.tif"))
+            if p == "MYD09GA": 
+                fp=glob.glob((data_folder+"MODIS/"+p+"_"+version+"/02_mosaic/"+d+"/02_*.tif"))
+            elif p == "MYDTBGA":fp=glob.glob((data_folder+"MODIS/"+p+"_006"+"/02_mosaic/"+d+"/02_*.tif"))
             elif p == "SIC": fp =glob.glob((data_folder+"AMSR2/sic_extracted/"+d+"/02_*tif"))
             imgs_4_fusion.append(fp)
         imgs=functools.reduce(lambda x,y:x+y,(imgs_4_fusion))
